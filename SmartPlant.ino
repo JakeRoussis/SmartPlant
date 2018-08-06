7f9dc3ba4b470f5f4057a9a0d9eb31f903591a97
JakeRoussis <40971419+JakeRoussis@users.noreply.github.com>
2018-08-06 21:14:28 +1000

    Uploaded SmartPlant.ino v1.0 & Data Screenshot
    Final version of SmartPlant featuring an extra colour and screenshot of raw soil data being read in for testing purposes.

--- SmartPlant.ino
+++ SmartPlant.ino
@@ -1,35 +1,37 @@
 /*
- * SmartPlant v0.1
- * 
- * Checks plant soil moisture and alerts user when they need to water it.
- * 
- * Checks soil moisture level against a pre-determined 'suitable' value that was tested
- * and take this value to test it against the current soil condition. This is then
- * passed through an if statement that changes the colour of the LED for the situation.
- * 
- * RED - Plant requires watering
- * BLUE - Plant has just been watered/has too much moisture
- * Green - Plant is at ideal range
- * 
- * Created 6/08/2018 
- * by Jake Roussis
- * 
- * Current 'waterMe' values are only test values in order to determine
- * LED functionality and set-up. Correct values will be in v1.0
- * 
- */
+   SmartPlant v1.0
+
+   Checks plant soil moisture and alerts user when they need to water it.
+
+   Checks soil moisture level against a pre-determined 'suitable' value that was tested
+   and take this value to test it against the current soil condition. This is then
+   passed through an if statement that changes the colour of the LED for the situation.
+
+   RED - Plant requires watering #RANGE: 0-50
+   BLUE - Plant has just been watered/has too much moisture #RANGE: 701+
+   Green - Plant is at ideal range #RANGE: 201-700
+   Yellow - Needing water soon #RANGE: 51-200
+
+   Created 6/08/2018
+   by Jake Roussis
+
+   Dry soil was tested to measure no more than 40. As a result 50 is the threshold for requiring water.
+   Wet soil tested between 200 and roughly 660 as a result the range for wet was 200-700
+   Soaked soil or just watered was 700+
+
+*/
 
 int val = 0; //Store moisture value
 int soilPin = A0; //Variable for sensor
 int soilPower = 7; //Variable for sensor power
-int waterMe = 450; //Minimum level for when the plant needs water
+int waterMe = 50; //Minimum level for when the plant needs water
 
 int redPin = 11; //RGB Red pin
 int greenPin = 10; //RGB green pin
 int bluePin = 9; //RGB blue pin
 
 void setup() {
-  Serial.begin(9600); //Open serial over USB
+ //DEBUGGING LOGGING// Serial.begin(9600); //Open serial over USB
   pinMode(soilPower, OUTPUT); //Initialize
   digitalWrite(soilPower, LOW); // Set to low so no power is flowing through sensor
 
@@ -54,14 +56,19 @@ void setColour(int redValue, int greenValue, int blueValue) {
 }
 
 void loop() {
+/*
+ *DEBUGGING LOGGING
   Serial.print("Soil Moisture = ");
   Serial.println(readSoil());
+*/ 
   delay(1000); // Take a reading every once every second
 
   if (readSoil() < waterMe) {
-    setColour(2550, 0, 0); //Requires watering
-  } else if (readSoil() > 850) {
-    setColour(0, 0, 255); //A lot of water detected
+    setColour(255, 0, 0); //Requires watering
+  } else if (readSoil() < 200) {
+  setColour(255, 255, 0);
+  } else if (readSoil() > 700) {
+  setColour(0, 0, 255); //A lot of water detected
   } else {
     setColour(0, 255, 0); //Perfect range of water
   }
